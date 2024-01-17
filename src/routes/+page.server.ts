c862796cd487f7aba52e1a4f3cce556a9d8cb6c9
Kureans <leonardtengys@gmail.com>
2024-01-18 00:29:30 +0800

    Initialise repo

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+// import * as db from '$lib/index.ts';
+
+export const actions = {
+    default: async ({request}) => {
+        const data = await request.formData();
+        // const queryString = data.get("query");
+
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>PriceHist</h1>
+
+<p>It's the Wayback Machine of Ecommerce Goods.</p>
+
+<form method="POST">
+    <label>
+        Search for a product and see its price over time:
+        <input
+            type="text"
+            id="query"
+            name="query"
+        />
+    </label>
+</form>
